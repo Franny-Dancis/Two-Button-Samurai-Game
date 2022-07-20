8954c4044a6e48e67a3a333b88c598ef10a3c048
Franny-Dancis <42708672+Franny-Dancis@users.noreply.github.com>
2022-07-20 08:37:25 -0700

    Continued working on functionality, added point tracker

--- One Button Game.gd
+++ One Button Game.gd
@@ -1,56 +1,27 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var pointCounter = $Points
+var points = 0
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_Sweet_Spot_area_entered(area):
-	print("Sweet Spot Entered")
-
-
-func _on_Sweet_Spot_area_exited(area):
-	print("Sweet Spot Exited")
-
-
-func _on_Fail2_area_entered(area):
-	print("Fail Spot Entered")
-
-
-func _on_Fail2_area_exited(area):
-	print("Sweet Spot Exited")
-
-
-func _on_Fail_area_entered(area):
-	print("Fail Spot Entered")
-
-
-func _on_Fail_area_exited(area):
-	print("Sweet Spot Exited")
-
-
-func _on_Success_2_area_exited(area):
-	print("Success Spot Exited")
-
-
-func _on_Success_area_exited(area):
-	print("Success Spot Exited")
-
-
-func _on_Success_2_area_entered(area):
-	print("Success Spot Entered")
-
-
-func _on_Success_area_entered(area):
-	print("Success Spot Entered")
+func _physics_process(delta):
+	if Input.is_action_just_pressed("Attack"):
+		$Slider/AnimationPlayer.stop()
+		if $Slider/Hitbox.overlaps_area($"GaugeNoCrit/Sweet Spot"):
+			points += 10
+			print("Great hit!")
+		if $Slider/Hitbox.overlaps_area($"GaugeNoCrit/Fail2"):
+			points += 0
+			print("Fail.")
+		if $Slider/Hitbox.overlaps_area($"GaugeNoCrit/Fail"):
+			points += 0
+			print("Fail.")
+		if $Slider/Hitbox.overlaps_area($"GaugeNoCrit/Success"):
+			points += 5
+			print("Success!")
+		if $Slider/Hitbox.overlaps_area($"GaugeNoCrit/Success 2"):
+			points += 5
+			print("Success!")
+	pointCounter.text = str(points)
+	$Slider/AnimationPlayer.play("Move")
